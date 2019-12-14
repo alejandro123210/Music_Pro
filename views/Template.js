4d190828a5cee3c008dbde7611f236c1daced174
Alejandro Gonzalez <myname123210@gmail.com>
2019-12-13 19:08:02 -0500

    template added

---
+++ views/Template.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import * as firebase from 'firebase';
+
+class ClassName extends React.Component {
+
+  render() {
+    return (
+      // this is just random filler for the template, but this is where what the user sees is rendered
+      <View />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+});
+
+
+//this lets the component get imported other places
+export default ClassName;
